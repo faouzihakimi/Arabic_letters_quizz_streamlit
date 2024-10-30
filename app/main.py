3152800ee7e09c4e858dcf38d7852518f04aae7b
Faouzi <faouzi.hakimi@yahoo.com>
2024-10-30 19:05:03 +0100

    arabic letter update

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import cv2
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
+import tensorflow as tf
 import numpy as np
 import joblib
 import random
@@ -10,20 +11,59 @@ import sys
 sys.path.append('../')
 
 
-stroke_width = 50
+stroke_width = 20
 stroke_color = "#000"
 bg_color = "#fff"
 drawing_mode = "freedraw"
 realtime_update = True
 
+arabic_phonetic = {
+    'ا': 'alif',
+    'ب': 'baa',
+    'ت': 'taa',
+    'ث': 'thaa',
+    'ج': 'jeem',
+    'ح': 'ḥaa',      # Using ḥ to denote the emphatic sound
+    'خ': 'khā',      # Using khā for the emphatic sound
+    'د': 'dāl',
+    'ذ': 'dhāl',     # Using dhāl for the emphatic sound
+    'ر': 'rā',
+    'ز': 'zayn',
+    'س': 'sīn',
+    'ش': 'shīn',
+    'ص': 'ṣād',      # Using ṣ to denote the emphatic sound
+    'ض': 'ḍād',      # Using ḍ to denote the emphatic sound
+    'ط': 'ṭā',       # Using ṭ to denote the emphatic sound
+    'ظ': 'ẓā',       # Using ẓ to denote the emphatic sound
+    'ع': 'ʿayn',     # Using ʿ for the voiced pharyngeal fricative
+    'غ': 'ghayn',
+    'ف': 'fā',
+    'ق': 'qāf',
+    'ك': 'kāf',
+    'ل': 'lām',
+    'م': 'mīm',
+    'ن': 'nūn',
+    'ه': 'hā',       # Keeping this as is since it is distinct
+    'و': 'wāw',
+    'ي': 'yā'
+}
+
+
+
+
+
+
+# Create a DataFrame from the dictionary
+df_arabic = pd.DataFrame(list(arabic_phonetic.items()), columns=['Arabic Letter', 'Phonetic'])
+
 def rgb2gray(rgb):
     return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
 
 # Define the getSum function
-def getSum(n):
-    while n > 9:
-        n = sum(int(digit) for digit in str(n))
-    return n
+# def getSum(n):
+#     while n > 9:
+#         n = sum(int(digit) for digit in str(n))
+#     return n
 
 def initialize_session_state():
     if 'current_question' not in st.session_state:
@@ -31,114 +71,161 @@ def initialize_session_state():
     if 'player_score' not in st.session_state:
         st.session_state.player_score = 0
     if 'seed' not in st.session_state:
-        current_date = datetime.date.today()
-        date_number = int(current_date.strftime("%Y%m%d"))
-        st.session_state.seed = date_number
+        st.session_state.seed = random.randint(0, 1000)
+    if 'answer_check' not in st.session_state:
+        st.session_state.answer = 0 #binary
+    if 'question_number' not in st.session_state:
+        st.session_state.question_number = 0
+
 
 # Function to generate questions
-def generate_questions(number_list):
-    questions = []
-    for number in number_list:
-        question = "Quelle est la somme des chiffres de " + str(number) + "? "
-        correct_answer = getSum(number)
-        questions.append({'question': question, 'correct_answer': correct_answer})
-    return questions
+# def generate_questions_archive(number_list):
+#     questions = []
+#     for number in number_list:
+#         question = "Quelle est la somme des chiffres de " + str(number) + "? "
+#         correct_answer = getSum(number)
+#         questions.append({'question': question, 'correct_answer': correct_answer})
+#     return questions
 
-def update_score(sorted_answers,probas, correct_answer, question_number):
+def generate_question():
+    
+    random_number = random.randint(0, len(df_arabic['Phonetic']) - 1)
+    question = "How do you write the arabic letter '" + df_arabic['Phonetic'][random_number] + "' ? :writing_hand:"
+    
+
+    return {'question': question, 'correct_answer': random_number}
 
 
-    if probas[0] <2/9:
-        st.write("Réponse non reconnue.")
+# def update_score(sorted_answers,probas, correct_answer, question_number):
 
 
-    if int(sorted_answers[0]) == correct_answer:
-        st.subheader("Bonne réponse ! :white_check_mark:" ) 
-        st.session_state.player_score += 1
+#     if probas[0] <2/9:
+#         st.write("Réponse non reconnue.")
 
-    elif (int(sorted_answers[1]) == correct_answer) & (probas[1] >= 0.5*probas[0]):
-        st.subheader("Bonne réponse ! :white_check_mark:" ) 
-    else:
-        st.subheader("Mauvaise réponse. La bonne réponse pour "  + str(question_number) + " est " +    str(correct_answer) +'.')
+
+#     if int(sorted_answers[0]) == correct_answer:
+#         st.subheader("Bonne réponse ! :white_check_mark:" ) 
+#         st.session_state.player_score += 1
+
+#     elif (int(sorted_answers[1]) == correct_answer) & (probas[1] >= 0.5*probas[0]):
+#         st.subheader("Bonne réponse ! :white_check_mark:" ) 
+#     else:
+#         st.subheader("Mauvaise réponse. La bonne réponse pour "  + str(question_number) + " est " +    str(correct_answer) +'.')
 
 # Generate questions based on a random list of numbers
 
 def page1():
-    st.title("Maths et dessins 	:writing_hand:")
-    st.header("Additionne les chiffres du nombre affiché jusqu'à obtenir un chiffre entre 1 et 9 !")
-    if st.button("Jouer !"):
-        st.session_state.page = 2
+    st.title("Learn the arabic letters	:writing_hand:")
+    
+    if st.session_state.question_number ==0:
+            if st.button("Play ! :pencil2:"):
+                st.session_state.page = 2
+                st.experimental_rerun() #to avoid the double click issue
+    else:
+        st.subheader("Your score: " + str(st.session_state.player_score) + " / " + str(st.session_state.question_number))
+
+        if st.button("Play again !"):
+            st.session_state.page = 2
+            st.experimental_rerun() #to avoid the double click issue
+
+        
+
 
 
 
 
 def page2():
 
-    initialize_session_state()
-    random.seed(st.session_state.seed)
-    number_list = random.sample(range(1, 1000), 10)
-    quiz_questions = generate_questions(number_list)
-    current_question = quiz_questions[st.session_state.current_question]
     
+    random.seed(st.session_state.seed)
+    st.session_state.current_question = generate_question()
+    st.subheader(st.session_state.current_question['question'])
+    
+    #st.write(f"Ton score: {st.session_state.player_score} / {len(quiz_questions)}")      
 
-    if st.session_state.current_question < len(quiz_questions)-1:
-        st.subheader(quiz_questions[st.session_state.current_question]['question'])
-        st.write(f"Ton score: {st.session_state.player_score} / {len(quiz_questions)}")      
-    else:
-            st.write("Merci d'avoir joué !")
-            st.write("Score final: " + str(st.session_state.player_score) + " / " + str(len(quiz_questions)))
-            
-            if st.button("Nouvelle partie"):
-                st.session_state.player_score = 0
-                st.session_state.current_question = 0
-                st.session_state.seed = st.session_state.seed + 1000
-
+    col1, col2 = st.columns(2, gap="small")
     #col1, col2 = st.columns(2
-    canvas_result = st_canvas(
-        fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
-        stroke_width=stroke_width,
-        stroke_color=stroke_color,
-        background_color=bg_color,
-        background_image=None,
-        update_streamlit=realtime_update,
-        height=300,
-        width= 300,
-        drawing_mode=drawing_mode,
-        point_display_radius=0,
-        display_toolbar= True,
-        key="full_app",
-    )
+    with col1:
+        canvas_result = st_canvas(
+            fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+            stroke_width=stroke_width,
+            stroke_color=stroke_color,
+            background_color=bg_color,
+            background_image=None,
+            update_streamlit=realtime_update,
+            height=320,
+            width= 320,
+            drawing_mode=drawing_mode,
+            point_display_radius=0,
+            display_toolbar= True,
+            key="full_app",
+        )
 
-    #preprocessing of the image
-    img_width = 8
-    try:
-        gray_array = rgb2gray(canvas_result.image_data)
-        image_resized = cv2.resize(gray_array, (img_width,img_width), interpolation = cv2.INTER_AREA)
-        image_resized = abs(image_resized -np.max(image_resized))/max(np.max(image_resized),1)
-    except:
-        image_resized = np.zeros((img_width,img_width), dtype = np.uint8)
-    #st.dataframe(image_resized)
-    if sum(sum(image_resized)) > img_width*0.1:
-        #model = joblib.load('xgb.pkl')
-        model = joblib.load('app/model_svm.pkl')
-        processed_im = image_resized.reshape((1, -1))
-        prediction = model.predict_proba(processed_im)
-        prediction = prediction[0]        
-        probas = pd.DataFrame(prediction).sort_values(ascending=False, by=0).values
-        sorted_answers = pd.DataFrame(prediction).sort_values(ascending=False, by=0).index
-
-        #st.write('Ta réponse : ' + str(sorted_answers)) #For test/maintenance only
-        #st.write('Ta réponse : ' + str(probas)) #For test/maintenance only
-    # else:
-    #     sorted_answers = None
-
-        if st.session_state.current_question < len(quiz_questions)-1:
-            if probas[0] <1/3 or (probas[2] + probas[3] > probas[0]): #corresponding to 3 times the probability of a random answer
-                st.write("Réponse non reconnue.")
+        #preprocessing of the image
+        img_width = 32
+        try:
+            gray_array = rgb2gray(canvas_result.image_data)
+            image_resized = cv2.resize(gray_array, (img_width,img_width), interpolation = cv2.INTER_AREA)
+            image_resized =  1 - image_resized.astype('float32') / 255
+            image_resized = image_resized.T #train images are transposed
+        except:
+            image_resized = np.zeros((img_width,img_width), dtype = np.uint8)
+    with col2:
+        if st.button("Check :white_check_mark:"):
+            if sum(sum(image_resized)) > img_width*0.1:
+                #st.write(str(sum(sum(image_resized))))
+                model = tf.keras.models.load_model('app/cnn_model.h5')
+                image_processed = image_resized.reshape(1, img_width, img_width, 1)
+                probas = model.predict(image_processed)
+                max_proba = np.max(probas)
+                answer = np.argmax(probas, axis=1)[0]
+                
+                if max_proba <1/2 : #corresponding to 3 times the probability of a random answer
+                    st.write("Unrecognized answer. Please try again.")
+                else:
+                    #st.write("Reponse: " + str(df_arabic['Phonetic'][answer]))
+                    #st.write("Answer: " + str(df_arabic['Phonetic'][st.session_state.current_question['correct_answer']]))
+                    
+                        st.session_state.answer_check = (answer == st.session_state.current_question['correct_answer'])
+                        st.session_state.question_number += 1
+                        st.session_state.player_score += st.session_state.answer_check
+                        st.session_state.page = 3
+                        st.experimental_rerun() #to avoid the double click issue
             else:
-                #st.subheader("Réponse reconnue: " + str(sorted_answers[0]))
-                if st.button("Répondre"):
-                    update_score(sorted_answers,probas, current_question['correct_answer'], number_list[st.session_state.current_question])
-                    st.session_state.current_question += 1
+                st.write("Please write something.")
+
+        if st.button("Quit", type='primary'):
+            st.session_state.page = 1
+            st.experimental_rerun()
+    if st.button("Help 	:question:"):
+        #Display df_arabic
+        st.dataframe(df_arabic)                
+
+def page3():
+
+    st.session_state.seed = st.session_state.seed + 1
+    if  st.session_state.answer_check:     
+        st.subheader("Good answer !  	:+1: " )
+    else:
+        st.subheader("Wrong answer. :x:")
+        #st.subheader("La lettre écrite est un " + str(df_arabic['Phonetic'][st.session_state.answer]) + " et non pas " + str(st.session_state.current_question['correct_answer']))
+        #st.write("Reponse donnée: " + str(df_arabic['Phonetic'][st.session_state.answer]))
+        #st.write("Bonne réponse: " + str(st.session_state.current_question['correct_answer']))
+    
+    st.subheader("The letter " + str(df_arabic['Phonetic'][st.session_state.current_question['correct_answer']]) + " is written as " + str(df_arabic['Arabic Letter'][st.session_state.current_question['correct_answer']]) )      
+    
+    st.write("Your score: " + str(st.session_state.player_score) + " / " + str(st.session_state.question_number))
+    
+    if st.button("Continue"):
+        
+        st.session_state.page = 2
+        st.experimental_rerun()
+    elif st.button("Quit", type='primary'):
+        st.session_state.page = 1
+        st.experimental_rerun()
+            
+
+
 
 
     
@@ -146,13 +233,18 @@ def page2():
 
 
 def main():
+
+    # Initialize session state
     if "page" not in st.session_state:
         st.session_state.page = 1
-
+    initialize_session_state()
     if st.session_state.page == 1:
         page1()
     elif st.session_state.page == 2:
         page2()
+
+    elif st.session_state.page == 3:
+        page3()
 
 
 if __name__ == "__main__":
